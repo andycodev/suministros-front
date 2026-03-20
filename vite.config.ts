867aa9f3c21396a7526cdfadd1a750db98a58d25
andyvega <andy.vega@adventistas.org>
2026-03-20 12:31:47 -0500

    Image Docker Success Develop and Production

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,25 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  // 1. Asegura que las rutas de los assets sean absolutas
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
   server: {
-    host: '0.0.0.0', // Esto expone el proyecto a toda tu red local
-    port: 5173,      // Este es el puerto por defecto, puedes asegurarlo aquí
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    port: 5173,
+    // 2. Esto es clave para que al recargar en /order funcione
+    strictPort: true,
+  },
+  // 3. Configuración para que el navegador no se pierda al recargar
+  preview: {
+    port: 5173,
+    strictPort: true,
   }
 })
