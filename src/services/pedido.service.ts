a5ea2736fea7db8c95da8efebe0638695e9f1bd2
andyvega <andy.vega@adventistas.org>
2026-03-24 14:15:34 -0500

    refactor services

--- src/services/pedido.service.ts
+++ src/services/pedido.service.ts
@@ -3,7 +3,7 @@ import baseApi from '@/api/baseApi';
 const endPointUrl = '/api/suministros';
 
 export const getPeriodosFn = async () => {
-    const { data } = await baseApi.get(`${endPointUrl}/periodos`);
+    const { data: { data } } = await baseApi.get(`${endPointUrl}/periodos`);
     return data;
 }
 
